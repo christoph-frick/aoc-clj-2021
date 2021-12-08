d2b90f6fda5855d038eab1eb498332bc23b3c511
Christoph Frick <cf@ofnir.net>
2021-12-08 21:33:14 +0100

    Day 8, part 2

--- src/aoc_2021/day_08.clj
+++ src/aoc_2021/day_08.clj
@@ -1,7 +1,8 @@
 (ns aoc-2021.day-08
   (:require [aoc.file :as file]
             [aoc.convert :as c]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (defn parse-line
   [s]
@@ -13,6 +14,54 @@
   [s]
   (map parse-line (str/split-lines s)))
 
+(defn cnt-pred
+  [x]
+  (fn [_ input]
+    (= x (count input))))
+
+(defn cnt-mask-cnt-pred
+  [initial-count mask count-after-mask]
+  (fn [solution input]
+    (let [to-mask (solution mask)]
+      (and
+       (= initial-count (count input))
+       (= count-after-mask (count (set/difference input to-mask)))))))
+
+(defn solution
+  [{:keys [inputs] :as puzzle}]
+  (let [solution (reduce (fn [{:keys [inputs solution]} [nr f]]
+                           (let [split (group-by (partial f solution) inputs)]
+                             {:inputs (split false)
+                              :solution (assoc solution nr (first (split true)))}))
+                         {:inputs inputs
+                          :solution {}}
+                         (partition 2 [1 (cnt-pred 2)
+                                       7 (cnt-pred 3)
+                                       4 (cnt-pred 4)
+                                       8 (cnt-pred 7)
+                                       9 (cnt-mask-cnt-pred 6 4 2)
+                                       2 (cnt-mask-cnt-pred 5 4 3)
+                                       3 (cnt-mask-cnt-pred 5 1 3)
+                                       5 (cnt-mask-cnt-pred 5 1 4)
+                                       0 (cnt-mask-cnt-pred 6 1 4)
+                                       6 (cnt-mask-cnt-pred 6 1 5)]))]
+    (assoc puzzle
+           :solution (into {}
+                           (map (fn [[k v]]
+                                  [v k]))
+                           (:solution solution)))))
+
+(defn number
+  [{:keys [solution number]}]
+  (->> number
+       (map solution)
+       (str/join)
+       (c/stol)))
+
+(defn solve
+  [puzzle]
+  (number (solution puzzle)))
+
 ; right now i have no clue what this would help for part-2...
 (defn solution-1
   [s]
@@ -23,6 +72,13 @@
        (filter #{2 3 4 7})
        (count)))
 
+(defn solution-2
+  [s]
+  (->> s
+       (parse)
+       (map (comp number solution))
+       (apply +)))
+
 (def input "day/08/input.txt")
 
 (defn part-1
@@ -31,4 +87,4 @@
 
 (defn part-2
   []
-  nil)
+  (solution-2 (file/read input)))
--- test/aoc_2021/day_08_test.clj
+++ test/aoc_2021/day_08_test.clj
@@ -30,8 +30,29 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 (deftest test-solution-1
   (is (= 26 (sut/solution-1 input))))
 
+(deftest test-solution
+  (let [puzzle (sut/parse-line input1)
+        solution (:solution (sut/solution puzzle))]
+    (is (= {#{\c \a \g \e \d \b} 0,
+            #{\a \b} 1,
+            #{\g \c \d \f \a} 2,
+            #{\f \b \c \a \d} 3,
+            #{\e \a \f \b} 4,
+            #{\c \d \f \b \e} 5,
+            #{\c \d \f \g \e \b} 6,
+            #{\d \a \b} 7,
+            #{\a \c \e \d \g \f \b} 8,
+            #{\c \e \f \a \b \d} 9}
+           solution))))
+
+(deftest test-solve
+  (is (= 5353 (-> input1 sut/parse-line sut/solve))))
+
 (deftest test-part-1
   (is (= 367 (sut/part-1))))
 
-(deftest ^:kaocha/pending test-part-2
-  (is (= 42 (sut/part-2))))
+(deftest test-solution-2
+  (is (= 61229 (sut/solution-2 input))))
+
+(deftest test-part-2
+  (is (= 974512 (sut/part-2))))
